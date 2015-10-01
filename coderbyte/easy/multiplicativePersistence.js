c5a528585d082c461e1117526c5bc6371263c23e
Andrew Smith <andrewsm@amazon.com>
2015-10-01 08:22:02 -0700

    yah!

---
+++ coderbyte/easy/multiplicativePersistence.js
@@ -0,0 +1,24 @@
+/**
+ * Have the function MultiplicativePersistence(num) take the num parameter 
+ * being passed which will always be a positive integer and return its 
+ * multiplicative persistence which is the number of times you must multiply 
+ * the digits in num until you reach a single digit. For example: if num is 
+ * 39 then your program should return 3 because 3 * 9 = 27 then 2 * 7 = 14 and 
+ * finally 1 * 4 = 4 and you stop at 4. 
+ */
+
+function multiplicativePersistence(num) {
+  function recur(num, times) {
+    var numStr = num.toString();
+    var productOfDigits;
+    if (numStr.length > 1) {
+      productOfDigits = numStr.split('').reduce(function(acc, val) {
+        return acc * parseInt(val, 10) 
+      }, 1);
+      return recur(productOfDigits.toString(), ++times);
+    }
+    return times;
+  }
+  
+  return recur(num, 0);
+}
